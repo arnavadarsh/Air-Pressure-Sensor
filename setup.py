b6d3f991aea37aff00ae7e1ffbd8e5f2d260fc09
arnavadarsh <162832692+arnavadarsh@users.noreply.github.com>
2026-01-15 23:23:29 +0530

    Fresh start: push full VS Code project

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+#changes
+
+
+
+def get_requirements()-> List[str]:
+  """This function will return the list of requirements"""
+  requirements_list = List[str] = []
+
+
+
+  return requirements_list
+  
+setup(
+  name="sensor",
+  version="0.0.1",
+  author="Arnav Adarsh",
+  author_email="arnavadarsh26@gmail.com",
+  packages=find_packages(),
+  intall_requires=get_requirements()
+)
